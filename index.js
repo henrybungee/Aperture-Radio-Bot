947e1434af61f5f4e8b13021a60e16853edb64bd
Gnelf <s018373@students.lmsd.org>
2020-01-13 16:44:15 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+//https://discordapp.com/oauth2/authorize?&client_id=666315424605929472&scope=bot&permissions=70703184
+
+const Discord = require('discord.js');
+const client = Discord.Client();
+
+
+
+client.login('NjY2MzE1NDI0NjA1OTI5NDcy.XhyaCA.Qs5RqrtddRIU3QAR5Ah19Begpr4');
